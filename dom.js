e3a719fa4499b700b063e3a0571311bced2e84a5
aabansyed <aabantech76@gmail.com>
2025-04-27 06:36:05 +0500

    Update dom.js

--- dom.js
+++ dom.js
@@ -6,3 +6,18 @@ document.addEventListener("DOMContentLoaded", function () {
       navList.classList.toggle("open"); // Adds/removes the class to slide in menu
     });
   });
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+});
